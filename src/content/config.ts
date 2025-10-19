29d0bec57fe5b47996a6ffd1b73bef6255824256
Viacheslav Azarov <public@religiosa.ru>
2025-10-19 23:23:02 +0200

    Опечатка в рутах рецептов

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,6 @@
-import { z, defineCollection } from 'astro:content';
+import { z, defineCollection } from "astro:content";
+import { articleSchema } from "~/models/Article";
 import { ingredientsSchema } from "~/models/Ingredient";
-import { articleSchema } from '~/models/Article';
 
 const recipeCollection = defineCollection({
 	schema: z.object({
@@ -9,22 +9,19 @@ const recipeCollection = defineCollection({
 		recommended: z.array(z.string()).optional(),
 		time: z.string(), // TODO regex
 		waiting: z.string().optional(), //TODO regex
-		ingredients: z.union([
-			z.record(z.array(ingredientsSchema)),
-			z.array(ingredientsSchema),
-		]),
+		ingredients: z.union([z.record(z.array(ingredientsSchema)), z.array(ingredientsSchema)]),
 		sources: z.array(z.string()).optional(),
 	}),
 });
 
 const articlesCollection = defineCollection({
-	schema: articleSchema
+	schema: articleSchema,
 });
 
 export const collections = {
-	'recepies': recipeCollection,
-	'electronics': articlesCollection,
-	'polski': articlesCollection,
-	'programming': articlesCollection,
-	'woodworking': articlesCollection,
+	recipes: recipeCollection,
+	electronics: articlesCollection,
+	polski: articlesCollection,
+	programming: articlesCollection,
+	woodworking: articlesCollection,
 };
